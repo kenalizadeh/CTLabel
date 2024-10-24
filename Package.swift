bbdbf8f154fcd505f9f77b9871b3b810efd6d9df
Kenan Alizadeh <kananalizade@gmail.com>
2024-10-24 22:25:17 +0400

    package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 let package = Package(
     name: "CTLabel",
     platforms: [.iOS(.v10)],
+    path: "CTLabel/Classes/",
     products: [
         .library(name: "CTLabel", targets: ["CTLabel"]),
     ],
